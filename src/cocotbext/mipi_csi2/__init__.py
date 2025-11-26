43e52560946d52c017662f884d2ebbcb883a92eb
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-11-27 02:45:22 +0530

    __init__.py

--- src/cocotbext/mipi_csi2/__init__.py
+++ src/cocotbext/mipi_csi2/__init__.py
@@ -1,3 +1,3 @@
-from .bus import Mipi_csi2Bus
-from .driver import Mipi_csi2Driver
-from .config import Mipi_csi2Config
+from .bus import Mipi_csi2Bus as Mipi_csi2Bus
+from .driver import Mipi_csi2Driver as Mipi_csi2Driver
+from .config import Mipi_csi2Config as Mipi_csi2Config
