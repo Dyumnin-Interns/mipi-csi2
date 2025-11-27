fe303252a7db56a766fabea843d205b1704d4211
mithaliratnakar <mithaliratnakar044@gmail.com>
2025-11-27 09:15:13 +0530

    bus.py

--- src/cocotbext/mipi_csi2/bus.py
+++ src/cocotbext/mipi_csi2/bus.py
@@ -1,7 +1,26 @@
 """Bus creator."""
 import cocotb
 from cocotb_bus import Bus
+_signals = [
+    "clk",
+    "resetn",
 
+    # Driver inputs
+    "lane0_byte",
+    "lane0_valid",
+    "lane1_byte",
+    "lane1_valid",
+    "txrequest_hs",
+    "txwrite_hs",
+    "sop",
+    "eop",
+
+    # DUT outputs
+    "txready_hs",
+    "TxDDRClkHS",
+    "D0",
+    "D1"
+]
 
 class Mipi_csi2Bus(Bus):
       """For most cases the defaault bus creator in cocotb_bus is ok. Some protocols have edge cases that need to be handled here.
@@ -10,7 +29,7 @@ class Mipi_csi2Bus(Bus):
       2. relationship between signals that need to be checked e.g. byte_enable == width_of(data)/8
       3. Depending on version/profile have different lists of signals.
       """
-      _signals:list[str]=[]
+      _signals = _signals
       def __init__(self, dut, prefix,
                    bus_separator="_", case_insensitive=False,
                    array_idx=None):
